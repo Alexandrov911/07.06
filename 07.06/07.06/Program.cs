2cf90d47df365a24e7d0f96f074d0ba948682904
Alexandrov911 <sashok17062002@mail.ru>
2021-06-18 13:14:42 +0300

    07.06

---
+++ 07.06/07.06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ВАРИАНТ 10
+            //1.Массив А вводится с клавиатуры. Сформировать новый массив В,
+            //состоящий из положительных элементов массива А.
+            //Размер произвольный.
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int[] b = new int[n];
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i]>0)
+                {
+                    b[k] = a[i];
+                    k = k + 1;
+                }
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 07.06/2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ВАРИАНТ 14
+            //1.Массив А вводится с клавиатуры.
+            //Сформировать новый массив В, состоящий из четных элементов массива А.
+            //Элементы вводятся с клавиатуры. Размер n.
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int[] b = new int[n];
+          
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    b[k] = a[i];
+                    k = k + 1;
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
